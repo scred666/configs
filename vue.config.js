d22bbad996c9df4f623020603f6c85e15d17dcfe
scred <scred666@gmail.com>
2021-01-04 15:59:08 +0200

    add vue.config.js add webpack-fake.config.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import '@/assets/sass/helpers.sass';`
+      },
+      css: {
+        sourceMap: true
+      }
+    }
+  }
+}
